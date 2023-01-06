ce1c22a70bd6ff835eadf9f98dd83cf9d78d4510
gexposé <gammaexposure@protonmail.com>
2023-01-06 04:43:26 +0000

    Print payouts by year

--- synack/tools/getPayouts.py
+++ synack/tools/getPayouts.py
@@ -8,9 +8,19 @@ s1.gecko = False
 s1.Proxy = True
 s1.getSessionToken()
 
-fields = ['Date', 'Payout']
+def print_income(dct):
+    for year, amount in dct.items():
+        print("{}: ${:,.2f}".format(year, amount))
+
 payouts = s1.getTransactions()
-with open('synack_payouts.csv', 'w') as f:
-    write = csv.writer(f)
-    write.writerow(fields)
-    write.writerows(payouts)
+keys = payouts[0].keys()
+transactionDict = dict()
+for i in range(len(payouts)):
+    thisYear = str(payouts[i]['transactionDate'][0:4])
+    thisAmount = float(payouts[i]['amount'])
+    if thisYear in transactionDict:
+        tmpVal = transactionDict[thisYear]
+        transactionDict[thisYear] = float(tmpVal) + float(thisAmount)
+    else:
+        transactionDict[thisYear] = float(thisAmount)
+print_income(transactionDict)
