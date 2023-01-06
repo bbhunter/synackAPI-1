09c2ed9ce8841bee115cfd1d3a2d019fe9db3d86
gexposé <gammaexposure@protonmail.com>
2023-01-06 03:21:22 +0000

    Updated to fix csv writer

--- synack/tools/getPayouts.py
+++ synack/tools/getPayouts.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 from synack import synack
+import csv
 
 s1 = synack()
 s1.gecko = False
+s1.Proxy = True
 s1.getSessionToken()
 
+fields = ['Date', 'Payout']
 payouts = s1.getTransactions()
-for i in range(len(payouts)):
-    print(payouts[i])
+with open('synack_payouts.csv', 'w') as f:
+    write = csv.writer(f)
+    write.writerow(fields)
+    write.writerows(payouts)
